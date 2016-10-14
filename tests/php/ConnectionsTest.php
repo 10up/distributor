68a498e011bfa2263aafb153ae284d8ff395b3c3
Taylor Lovett <tlovett88@gmail.com>
2016-10-14 12:11:00 -0400

    More test files

---
+++ tests/php/ConnectionsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Syndicate;
+
+class ConnectionsTest extends \TestCase {
+	/**
+	 * Test connection registration
+	 * 
+	 * @since 0.8
+	 * @group Connections
+	 */
+	public function test_register() {
+		Connections::factory()->register( '\TestExternalConnection' );
+
+		$this->assertEquals( '\TestExternalConnection', Connections::factory()->get_registered()['test-external-connection'] );
+
+		Connections::factory()->register( '\TestInternalConnection', 'internal' );
+
+		$this->assertEquals( '\TestInternalConnection', Connections::factory()->get_registered()['test-internal-connection'] );
+	}
+}
---
+++ tests/php/ExternalConnectionTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Syndicate;
+
+class ExternalConnectionTest extends \TestCase {
+
+	/**
+	 * Text External Connection instantiation failure on no type
+	 *
+	 * @since  0.8
+	 * @group ExternalConnection
+	 */
+	public function test_instantiate_fail_type() {
+		Connections::factory()->register( '\TestExternalConnection' );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_type', true ),
+		    'return' => false,
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_url', true ),
+		    'return' => false,
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_auth', true ),
+		    'return' => false,
+		) );
+
+		\WP_Mock::userFunction( 'get_the_title', array(
+		    'times' => 1,
+		    'args' => 1,
+		    'return' => '',
+		) );
+
+		// Test non-existent connection ID
+		$external_connection = ExternalConnection::instantiate( 1 );
+
+		$this->assertEquals( 'external_connection_not_found', $external_connection->code );
+	}
+
+	/**
+	 * Text External Connection instantiation failure on not registered
+	 *
+	 * @since  0.8
+	 * @group ExternalConnection
+	 */
+	public function test_instantiate_fail_not_registered() {
+		Connections::factory()->register( '\TestExternalConnection' );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_type', true ),
+		    'return' => 'fake',
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_url', true ),
+		    'return' => 'fake',
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_auth', true ),
+		    'return' => false,
+		) );
+
+		\WP_Mock::userFunction( 'get_the_title', array(
+		    'times' => 1,
+		    'args' => 1,
+		    'return' => '',
+		) );
+
+		// Test non-existent connection ID
+		$external_connection = ExternalConnection::instantiate( 1 );
+
+		$this->assertEquals( 'no_external_connection_type', $external_connection->code );
+	}
+
+	/**
+	 * Text External Connection instantiation success
+	 *
+	 * @since  0.8
+	 * @group ExternalConnection
+	 */
+	public function test_instantiate_success() {
+		Connections::factory()->register( '\TestExternalConnection' );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_type', true ),
+		    'return' => 'test-external-connection',
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_url', true ),
+		    'return' => 'fake',
+		) );
+
+		\WP_Mock::userFunction( 'get_post_meta', array(
+		    'times' => 1,
+		    'args' => array( \WP_Mock\Functions::type( 'int' ), 'sy_external_connection_auth', true ),
+		    'return' => array(),
+		) );
+
+		\WP_Mock::userFunction( 'get_the_title', array(
+		    'times' => 1,
+		    'args' => 1,
+		    'return' => '',
+		) );
+
+		// Test non-existent connection ID
+		$external_connection = ExternalConnection::instantiate( 1 );
+
+		$this->assertTrue( is_a( $external_connection, '\Syndicate\ExternalConnection' ) );
+	}
+}
