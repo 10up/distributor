eb55dfca149df81340d77ed4ce34a9f7a13be830
Zane Kolnik <zane.kolnik@get10up.com>
2016-10-21 17:05:06 -0400

    Completing test for pull method, #2

--- tests/php/NetworkSiteConnectionsTest.php
+++ tests/php/NetworkSiteConnectionsTest.php
@@ -49,4 +49,39 @@ class NetworkSiteConnectionsTest extends \TestCase {
 
     }
 
+    /**
+     * Pull returns an array of Post IDs on success. This test simulates sending an
+     * array containing three IDs (integers) will receive an array containing
+     * three integers.
+     *
+     * @since  0.8
+     * @return
+     */
+    public function test_pull(){
+
+        $this->connection_obj->site->blog_id = 2;
+
+        \WP_Mock::userFunction( 'get_permalink' );
+
+        \WP_Mock::userFunction( 'get_post_meta', [
+            'return' => (object) [
+                'key' => []
+            ]
+        ] );
+
+        \WP_Mock::userFunction( 'get_post', [
+            'return' => (object) [
+                'post_tite' => 'My post title',
+                'meta'      => [],
+            ]
+        ] );
+
+        \WP_Mock::userFunction( 'wp_insert_post', [
+            'return' => [4, 3, 2]
+        ] );
+
+        $this->assertTrue( count( $this->connection_obj->pull( [ 2, 3, 4 ] ) ) === 3 );
+
+    }
+
 }
