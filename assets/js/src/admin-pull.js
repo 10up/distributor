d6e30470928142c288e52feb0ebcd35ec6caaf7d
Marco Pereirinha <marcopereirinha@MacBook-Pro-de-Marco.local>
2017-03-17 12:00:41 +0000

    Script logic to use heartbeat to lock pull screen

--- assets/js/src/admin-pull.js
+++ assets/js/src/admin-pull.js
@@ -1,8 +1,38 @@
 (function($) {
-	var chooseConnection = document.getElementById('pull_connections');
+	'use strict';
+
+	var chooseConnection = document.getElementById('pull_connections'),
+		modal = document.getElementById('dt-locked-screen'),
+		unlocked = false;
 
 	$(chooseConnection).on('change', function(event) {
 		document.location = event.currentTarget.options[event.currentTarget.selectedIndex].getAttribute('data-pull-url');
 	});
 
+	$(document).on('heartbeat-send', function(e, data) {
+		data.nonce = DtData.nonce;
+	});
+
+	$(document).on('heartbeat-tick', function(e, data) {
+		if ( true === data.unlocked && 'undefined' !== modal ) {
+			unlocked = true;
+			$(modal).dialog('close');
+		}
+	});
+
+	if ( 'undefined' !== modal ) {
+		$(modal).dialog({
+			dialogClass : 'wp-dialog',
+			title       : DtData.modal_title,
+			autoOpen    : true,
+			modal       : true,
+			draggable   : false,
+			resizable   : false,
+			close       : function() {
+				if ( true !== unlocked ) {
+					window.location = DtData.dashboard_url;
+				}
+			}
+		});
+	}
 })(jQuery);
