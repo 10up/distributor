9d5af410fe621dc5e91b737f85f55c196091105f
Adam Silverstein <adam@10up.com>
2018-01-19 12:57:25 -0500

    Oauth2 authentication fields UI, first pass

--- includes/classes/Authentications/WordPressDotcomOauth2Authentication.php
+++ includes/classes/Authentications/WordPressDotcomOauth2Authentication.php
@@ -7,7 +7,10 @@ use \Distributor\Authentication as Authentication;
 /**
  * This auth type is simple username/password WP style
  */
-class WordPressOath2Authentication extends Authentication {
+class WordPressDotcomOauth2Authentication extends Authentication {
+	static $slug                 = 'secret-key';
+	static $requires_credentials = true;
+	static $label                = 'Client ID/Secret';
 
 	public function __construct( $args ) {
 		parent::__construct( $args );
@@ -20,6 +23,30 @@ class WordPressOath2Authentication extends Authentication {
 	 * @since  0.8
 	 */
 	static function credentials_form( $args = array() ) {
+		global $wp;
+		$client_id = isset( $args['client_id'] ) ? $args['client_id'] : '';
+		?>
+		<p>
+			<label for="dt_client_id"><?php esc_html_e( 'Client ID', 'distributor' ); ?></label><br>
+			<input type="text" name="dt_external_connection_auth[client_id]" data-auth-field="client_id" value="<?php echo esc_attr( $client_id ); ?>" class="widefat auth-field" id="dt_client_id">
+		</p>
+		<?php
+		$client_secret = isset( $args['client_secret'] ) ? $args['client_secret'] : '';
+		?>
+		<p>
+			<label for="dt_client_secret"><?php esc_html_e( 'Client Secret', 'distributor' ); ?></label><br>
+			<input type="text" name="dt_external_connection_auth[client_secret]" data-auth-field="client_secret" value="<?php echo esc_attr( $client_secret ); ?>" class="widefat auth-field" id="dt_client_secret">
+		</p>
+		<?php
+				error_log(json_encode(admin_url( 'post-new.php?post_type=dt_ext_connection' ), JSON_PRETTY_PRINT));
+
+		$redirect_url = admin_url( 'post-new.php?post_type=dt_ext_connection' );
+		?>
+		<p>
+			<input type="hidden" name="dt_external_connection_auth[redirect_url]" data-auth-field="redirect_url" value="<?php echo esc_attr( $redirect_url ); ?>" class="widefat  auth-field" id="dt_redirect_url">
+		</p>
+
+		<?php
 	}
 
 	/**
