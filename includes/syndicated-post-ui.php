0804ac856fc8b1e89ddbb5188e4745718821ca3d
Taylor Lovett <tlovett88@gmail.com>
2018-02-26 13:05:33 -0500

    Fix replace revisions meta box warnings

--- includes/syndicated-post-ui.php
+++ includes/syndicated-post-ui.php
@@ -223,6 +223,10 @@ function new_revisions_meta_box( $post_id ) {
  * @since  1.0
  */
 function replace_revisions_meta_box( $post_type, $context, $post ) {
+	if ( empty( $post ) ) {
+		return;
+	}
+
 	$original_blog_id   = get_post_meta( $post->ID, 'dt_original_blog_id', true );
 	$original_post_id   = get_post_meta( $post->ID, 'dt_original_post_id', true );
 	$original_source_id = get_post_meta( $post->ID, 'dt_original_source_id', true );
