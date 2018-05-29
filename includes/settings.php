54b11ad165e065c2748543e145c6d99a3a81619f
Taylor Lovett <tlovett88@gmail.com>
2018-05-28 20:22:36 -0400

    Proper registration notice url

--- includes/settings.php
+++ includes/settings.php
@@ -271,6 +271,12 @@ function network_settings_screen() {
 
 	$license_key = ( ! empty( $settings['license_key'] ) ) ? $settings['license_key'] : '';
 	$email = ( ! empty( $settings['email'] ) ) ? $settings['email'] : '';
+
+	if ( DT_IS_NETWORK ) {
+		$notice_url = network_admin_url( 'admin.php?page=distributor-settings' );
+	} else {
+		$notice_url = admin_url( 'admin.php?page=distributor-settings' );
+	}
 	?>
 
 	<div class="wrap">
@@ -292,7 +298,7 @@ function network_settings_screen() {
 						</div>
 
 						<p class="description">
-							<?php echo wp_kses_post( __( 'Registration is 100% free and provides update notifications and upgrades inside the dashboard; <a href="https://distributorplugin.com/#cta">Register for your key</a>.', 'distributor' ) ); ?>
+							<?php echo wp_kses_post( sprintf( __( 'Registration is 100% free and provides update notifications and upgrades inside the dashboard; <a href="%s">Register for your key</a>.', 'distributor' ), esc_url( $notice_url ) ) ); ?>
 						</p>
 					</td>
 				</tr>
