23a30de021b561c80c590fae97af2f5e82b1850e
Stephanie Leary <steph@stephanieleary.com>
2018-07-03 11:59:07 -0500

    Add 'dt_original_post_parent' to post meta

--- includes/classes/ExternalConnections/WordPressExternalConnection.php
+++ includes/classes/ExternalConnections/WordPressExternalConnection.php
@@ -300,6 +300,7 @@ class WordPressExternalConnection extends ExternalConnection {
 
 			$post_props = get_object_vars( $post );
 			$post_array = array();
+			$original_post_parent = wp_get_post_parent_id( $item_array['post_id'] );
 
 			foreach ( $post_props as $key => $value ) {
 				$post_array[ $key ] = $value;
@@ -335,6 +336,7 @@ class WordPressExternalConnection extends ExternalConnection {
 			update_post_meta( $new_post, 'dt_original_post_url', esc_url_raw( $post_array['link'] ) );
 			update_post_meta( $new_post, 'dt_original_site_name', sanitize_text_field( $post_array['original_site_name'] ) );
 			update_post_meta( $new_post, 'dt_original_site_url', sanitize_text_field( $post_array['original_site_url'] ) );
+			update_post_meta( $new_post, 'dt_original_post_parent', (int) $original_post_parent );
 
 			if ( empty( $post_array['full_connection'] ) ) {
 				update_post_meta( $new_post, 'dt_full_connection', false );
@@ -443,6 +445,7 @@ class WordPressExternalConnection extends ExternalConnection {
 			'distributor_original_site_name' => get_bloginfo( 'name' ),
 			'distributor_original_site_url'  => home_url(),
 			'distributor_original_post_url'  => get_permalink( $post_id ),
+			'distributor_original_post_parent'  => wp_get_post_parent_id( $post_id ),
 			'distributor_remote_post_id'     => $post_id,
 			'distributor_signature'          => $signature,
 			'distributor_media'              => \Distributor\Utils\prepare_media( $post_id ),
@@ -728,7 +731,8 @@ class WordPressExternalConnection extends ExternalConnection {
 		$obj->media              = ( ! empty( $post['distributor_media'] ) ) ? $post['distributor_media'] : [];
 		$obj->original_site_name = ( ! empty( $post['distributor_original_site_name'] ) ) ? $post['distributor_original_site_name'] : null;
 		$obj->original_site_url  = ( ! empty( $post['distributor_original_site_url'] ) ) ? $post['distributor_original_site_url'] : null;
-
+		$obj->original_post_parent  = ( ! empty( $post['distributor_original_post_parent'] ) ) ? $post['distributor_original_post_parent'] : null;
+		
 		$obj->full_connection = ( ! empty( $post['full_connection'] ) );
 
 		/**
