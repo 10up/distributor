98cde6f7ec5332cf84c45e36eac08fdc3664bb38
Darin Kotter <darin.kotter@my.eitc.edu>
2018-08-24 14:33:28 -0600

    Add some javascript that will fire when a new post type is selected on the pull screen, that will reload the page with the proper post type selected.

--- assets/js/admin-pull.js
+++ assets/js/admin-pull.js
@@ -1,6 +1,7 @@
 import jQuery from 'jquery';
 
 const chooseConnection = document.getElementById( 'pull_connections' );
+const choosePostType = document.getElementById( 'pull_post_type' );
 
 jQuery( chooseConnection ).on( 'change', ( event ) => {
 
@@ -8,3 +9,15 @@ jQuery( chooseConnection ).on( 'change', ( event ) => {
 
 	document.body.className += ' ' + 'dt-loading';
 } );
+
+if ( chooseConnection && choosePostType ) {
+	jQuery( choosePostType ).on( 'change', ( event ) => {
+
+		const postType = event.currentTarget.options[ event.currentTarget.selectedIndex ].value;
+		const url = chooseConnection.options[ chooseConnection.selectedIndex ].getAttribute( 'data-pull-url' );
+
+		document.location = url + '&pull_post_type=' + postType;
+
+		document.body.className += ' ' + 'dt-loading';
+	} );
+}
