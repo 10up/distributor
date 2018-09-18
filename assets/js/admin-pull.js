ed29c7f86b92ce944e12e13ae5494062904f298b
Darin Kotter <darin.kotter@my.eitc.edu>
2018-09-18 08:39:16 -0600

    Make sure we take into account the status variable when choosing a post type.

--- assets/js/admin-pull.js
+++ assets/js/admin-pull.js
@@ -3,6 +3,7 @@ import jQuery from 'jquery';
 const chooseConnection = document.getElementById( 'pull_connections' );
 const choosePostType = document.getElementById( 'pull_post_type' );
 const choosePostTypeBtn = document.getElementById( 'pull_post_type_submit' );
+const form = document.getElementById( 'posts-filter' );
 
 jQuery( chooseConnection ).on( 'change', ( event ) => {
 
@@ -11,15 +12,22 @@ jQuery( chooseConnection ).on( 'change', ( event ) => {
 	document.body.className += ' ' + 'dt-loading';
 } );
 
-if ( chooseConnection && choosePostType && choosePostTypeBtn ) {
+if ( chooseConnection && choosePostType && choosePostTypeBtn && form ) {
 	jQuery( choosePostTypeBtn ).on( 'click', ( event ) => {
 
 		event.preventDefault();
 
 		const postType = choosePostType.options[ choosePostType.selectedIndex ].value;
 		const url = chooseConnection.options[ chooseConnection.selectedIndex ].getAttribute( 'data-pull-url' );
+		let status = 'new';
 
-		document.location = url + '&pull_post_type=' + postType;
+		if ( -1 < ( ' ' + form.className + ' ' ).indexOf( ' status-skipped ' ) ) {
+			status = 'skipped';
+		} else if ( -1 < ( ' ' + form.className + ' ' ).indexOf( ' status-pulled ' ) ) {
+			status = 'pulled';
+		}
+
+		document.location = url + '&pull_post_type=' + postType + '&status=' + status;
 
 		document.body.className += ' ' + 'dt-loading';
 	} );
