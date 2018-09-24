d6b44bdd3f942b610ca139107355fa5e60cea3d4
Taylor Lovett <tlovett88@gmail.com>
2018-09-24 09:57:10 -0400

    Add byline test

--- tests/wpassure/Settings.php
+++ tests/wpassure/Settings.php
@@ -46,4 +46,79 @@ class SettingsTest extends \WPAssure\PHPUnit\TestCase {
 		$I->dontSeeCheckboxIsChecked( '.form-table input[type="checkbox"]' );
 		$I->seeCheckboxIsChecked( '.form-table input[type="radio"][value="attached"]' );
 	}
+
+	/**
+	 * Test author bylines
+	 */
+	public function testAuthorBylineSetting() {
+		$I = $this->getAnonymousUser();
+
+		$I->loginAs( 'wpsnapshots' );
+
+		// First save the setting as checked
+		$I->moveTo( 'two/wp-admin/admin.php?page=distributor-settings' );
+
+		$I->waitUntilElementVisible( '#wpadminbar' );
+
+		$I->checkOptions( '.form-table input[type="checkbox"]' );
+
+		$I->click( '#submit' );
+
+		$I->waitUntilElementVisible( '#wpadminbar' );
+
+		// Now distribute a published post
+		$I->moveTo( 'wp-admin/post.php?post=40&action=edit' );
+
+		$I->waitUntilElementVisible( '#wp-admin-bar-distributor a' );
+
+		$I->moveMouse( '#wp-admin-bar-distributor a' );
+
+		$I->click( '#wp-admin-bar-distributor a' );
+
+		// Distribute post
+
+		$I->click( '#distributor-push-wrapper .new-connections-list .add-connection[data-connection-id="2"]' );
+
+		usleep( 500 );
+
+		$I->click( '#dt-as-draft' ); // Uncheck for publish, draft is checked by default
+
+		$I->click( '#distributor-push-wrapper .syndicate-button' );
+
+		$I->waitUntilElementVisible( '#distributor-push-wrapper .dt-success' );
+
+		// Now let's navigate to the new post
+
+		$I->moveTo( '/two/wp-admin/edit.php' );
+
+		$I->waitUntilElementVisible( '.wp-list-table' );
+
+		$I->click( '.wp-list-table tbody tr:nth-child(1) a.row-title' );
+
+		$I->waitUntilElementVisible( '#title' );
+
+		// Now let's check in the front end
+		$I->click( '#wp-admin-bar-view a' );
+
+		$post_url = $I->getCurrentUrl();
+
+		// Site One byline is shown
+		$I->seeText( 'Site One', '.byline .author' );
+
+		// Uncheck the setting
+		$I->moveTo( 'two/wp-admin/admin.php?page=distributor-settings' );
+
+		$I->waitUntilElementVisible( '#wpadminbar' );
+
+		$I->uncheckOptions( '.form-table input[type="checkbox"]' );
+
+		$I->click( '#submit' );
+
+		$I->waitUntilElementVisible( '#wpadminbar' );
+
+		// Verify byline is normal
+		$I->moveTo( $post_url );
+
+		$I->seeText( 'wpsnapshots', '.byline .author' );
+	}
 }
