32bf22c269bbae20faa12cd636e82ddff05125ba
Helen Hou-Sandi <helen.y.hou@gmail.com>
2018-10-08 15:08:54 -0600

    Remove `-dev` from the version string

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -1,8 +1,13 @@
 import gulp from 'gulp';
 import del from 'del';
+import replace from 'gulp-replace';
+import filter from 'gulp-filter';
 
 gulp.task( 'copy', () => {
+	const f = filter( 'distributor.php', { restore: true } );
+
 	del( ['./release/**/*'] );
+
 	gulp.src(
 		[
 			'composer.json',
@@ -14,5 +19,8 @@ gulp.task( 'copy', () => {
 			'vendor/yahnis-elsts/plugin-update-checker/**/*',
 		],
 		{ base: '.' } )
+		.pipe( f )
+		.pipe( replace( /-dev' \);/, '\' );' ) )
+		.pipe( f.restore )
 		.pipe( gulp.dest( 'release' ) );
 } );
