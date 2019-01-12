b8fb463685d8b303d85456e56795d040001cbd5f
Adam Silverstein <adam@10up.com>
2019-01-11 17:00:25 -0700

    OembedTests, first pass

---
+++ tests/wpacceptance/OembedTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Test oembeds.
+ *
+ * @package distributor
+ */
+
+/**
+ * PHPUnit test class.
+ */
+class OembedTests extends \TestCase {
+
+	/**
+	 * Test pushing content with an oEmbed.
+	 */
+	public function testOembedPushedContent() {
+		$I = $this->getAnonymousUser();
+
+		$I->loginAs( 'wpsnapshots' );
+
+		// Push post to connection 2.
+		$post_info = $this->pushPost( $I, 48, 2 );
+
+		$I->moveTo( $post_info['distributed_edit_url'] );
+
+
+		// Blog 3 is connection 2.
+		switch_to_blog( 3 );
+		$post = get_post( (int) $post_info['distributed_post_id'] );
+
+		// Make sure we see distributed time in publish box
+		$I->assertTrue( $post['post_content'] === 'https:\/\/twitter.com\/10up\/status\/1067517868441387008\r\n\r\n&nbsp;', 'oEmbed was sent properly, without being expanded.' );
+	}
+
+	/**
+	 * Test pulling content with an oEmbed.
+	 */
+	public function testOembedPulledContent() {
+		$I = $this->getAnonymousUser();
+
+		$I->loginAs( 'wpsnapshots' );
+
+		// Pull post to site 2
+	}
+
+}
