e8ad0487ef0b25bc422add34261ec3d35d739660
Adam Silverstein <adam@10up.com>
2019-01-13 21:31:44 -0700

    Test external pushing/pulling content with an oEmbed.

--- tests/wpacceptance/includes/TestCase.php
+++ tests/wpacceptance/includes/TestCase.php
@@ -17,10 +17,11 @@ class TestCase extends \WPAcceptance\PHPUnit\TestCase {
 	 * @param  int                     $post_id          Post ID to distributor
 	 * @param  int                     $to_connection_id Connection ID to distribute from
 	 * @param  string                  $from_blog_slug   Blog where original post lives. Empty string is main blog.
-	 * @param  string                  $post_status      New post status
+	 * @param  string                  $post_status      New post status.
+	 * @param  boolean                 $external         Is this an external connection push?
 	 * @return array
 	 */
-	protected function pushPost( \WPAcceptance\PHPUnit\Actor $I, $post_id, $to_connection_id, $from_blog_slug = '', $post_status = 'publish' ) {
+	protected function pushPost( \WPAcceptance\PHPUnit\Actor $I, $post_id, $to_connection_id, $from_blog_slug = '', $post_status = 'publish', $external = false ) {
 		$info = [
 			'original_edit_url' => $from_blog_slug . '/wp-admin/post.php?post=' . $post_id . '&action=edit',
 		];
@@ -54,21 +55,23 @@ class TestCase extends \WPAcceptance\PHPUnit\TestCase {
 
 		$I->waitUntilElementVisible( '#distributor-push-wrapper .dt-success' );
 
-		// Now let's navigate to the new post
+		// Now let's navigate to the new post - only works for network connections.
+		if ( ! $external ) {
 
-		$I->click( '#distributor-push-wrapper .new-connections-list .add-connection[data-connection-id="' . $to_connection_id . '"] a' );
+			$I->click( '#distributor-push-wrapper .new-connections-list .add-connection[data-connection-id="' . $to_connection_id . '"] a' );
 
-		$I->waitUntilElementVisible( '#wp-admin-bar-edit' );
+			$I->waitUntilElementVisible( '#wp-admin-bar-edit' );
 
-		$info['distributed_front_url'] = $I->getCurrentUrl();
+			$info['distributed_front_url'] = $I->getCurrentUrl();
 
-		$I->click( '#wp-admin-bar-edit a' );
+			$I->click( '#wp-admin-bar-edit a' );
 
-		$I->waitUntilElementVisible( '#title' );
+			$I->waitUntilElementVisible( '#title' );
 
-		$info['distributed_edit_url'] = $I->getCurrentUrl();
+			$info['distributed_edit_url'] = $I->getCurrentUrl();
 
-		$info['distributed_post_id'] = (int) $I->getElement( '#post_ID' )->getAttribute( 'value' );
+			$info['distributed_post_id'] = (int) $I->getElement( '#post_ID' )->getAttribute( 'value' );
+		}
 
 		return $info;
 	}
@@ -80,9 +83,11 @@ class TestCase extends \WPAcceptance\PHPUnit\TestCase {
 	 * @param  int                     $original_post_id Original post id
 	 * @param  int                     $to_blog_slug     Blog slug where post is being pulled in
 	 * @param  string                  $from_blog_slug   Blog we are pulling from. Empty string is main blog
+	 * @param  string                  $use_connection   The full connection name to use on the pull screen.
+	 *
 	 * @return array
 	 */
-	protected function pullPost( \WPAcceptance\PHPUnit\Actor $I, $original_post_id, $to_blog_slug, $from_blog_slug = '' ) {
+	protected function pullPost( \WPAcceptance\PHPUnit\Actor $I, $original_post_id, $to_blog_slug, $from_blog_slug = '', $use_connection = false ) {
 		if ( ! empty( $to_blog_slug ) ) {
 			$to_blog_slug .= '/';
 		}
@@ -96,6 +101,11 @@ class TestCase extends \WPAcceptance\PHPUnit\TestCase {
 		];
 
 		$I->moveTo( $to_blog_slug . 'wp-admin/admin.php?page=pull' );
+		error_log( '$use_connection' . $use_connection );
+		if ( $use_connection ) {
+			$I->selectOptions( '#pull_connections', $use_connection );
+			$I->waitUntilElementVisible( '.wp-list-table #cb-select-' . $original_post_id );
+		}
 
 		$I->checkOptions( '.wp-list-table #cb-select-' . $original_post_id );
 
@@ -103,7 +113,8 @@ class TestCase extends \WPAcceptance\PHPUnit\TestCase {
 
 		$I->waitUntilElementVisible( '#wpadminbar' );
 
-		$I->moveTo( $to_blog_slug . 'wp-admin/admin.php?page=pull&status=pulled' );
+		$I->click( '.pulled > a' );
+		$I->waitUntilElementVisible( '#wpadminbar' );
 
 		$I->moveMouse( '.wp-list-table tbody tr:nth-child(1) .page-title' );
 		$I->click( '.wp-list-table tbody tr:nth-child(1) .page-title .view a' );
