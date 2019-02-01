6f53dfa87cd2607498aaf0ac14c0d42bee4eaa6f
JayWood <jjwood2004@gmail.com>
2019-01-31 20:46:08 -0500

    Clean up distributed-post-ui.php file.

--- includes/distributed-post-ui.php
+++ includes/distributed-post-ui.php
@@ -33,11 +33,14 @@ function add_help_tab() {
 		return;
 	}
 
-	if ( empty( $_GET['post'] ) ) {
+	if ( empty( $_GET['post'] ) ) { // @codingStandardsIgnoreLine Nonce validation not necessary here.
 		return;
 	}
 
-	$connection_map = get_post_meta( intval( $_GET['post'] ), 'dt_connection_map', true );
+	// Set a post ID variable here for use a few times.
+	$post_id = intval( $_GET['post'] ); // @codingStandardsIgnoreLine Nonce not necessary, simply type-casting data here from the admin.
+
+	$connection_map = get_post_meta( $post_id, 'dt_connection_map', true );
 
 	if ( empty( $connection_map ) ) {
 		return;
@@ -45,13 +48,14 @@ function add_help_tab() {
 
 	$screen = get_current_screen();
 
-	$post_type_object = get_post_type_object( get_post_type( $_GET['post'] ) );
+	$post_type_object = get_post_type_object( get_post_type( $post_id ) );
 
 	// Add my_help_tab if current screen is My Admin Page
 	$screen->add_help_tab(
 		array(
 			'id'      => 'distributer',
 			'title'   => esc_html__( 'Distributor', 'distributor' ),
+			/* translators: %1$s: Post type singular name, %2$s: Post type singular name, %3$s: Pos type name */
 			'content' => '<p>' . sprintf( esc_html__( 'The number of connections this %1$s has been distributed to is shown in the publish meta box. If this %2$s is deleted, it could have ramifications across all those %3$s.', 'distributor' ), esc_html( strtolower( $post_type_object->labels->singular_name ) ), esc_html( strtolower( $post_type_object->labels->singular_name ) ), esc_html( strtolower( $post_type_object->labels->name ) ) ) . '</p>',
 		)
 	);
@@ -60,7 +64,7 @@ function add_help_tab() {
 /**
  * Output distributed to number
  *
- * @param  WP_Post $post Post object.
+ * @param  \WP_Post $post Post object.
  * @since  0.8
  */
 function distributed_to( $post ) {
@@ -83,6 +87,7 @@ function distributed_to( $post ) {
 
 	<div class="misc-pub-section curtime misc-pub-curtime">
 		<span id="distributed-to">
+			<?php /* translators: %d: number of connections */ ?>
 			<?php printf( wp_kses_post( _n( 'Distributed to <strong>%d</strong> connection', 'Distributed to <strong>%d</strong> connections', (int) $total_connections, 'distributor' ) ), (int) $total_connections ); ?>
 			<a class="open-distributor-help">(?)</a>
 		</span>
