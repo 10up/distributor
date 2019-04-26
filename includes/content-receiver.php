6d0bf6334d888feae1e4e518db76608458c89c46
avag <avag.novembit@gmail.com>
2019-04-26 18:31:00 +0400

    Fix linter errors

--- includes/content-receiver.php
+++ includes/content-receiver.php
@@ -14,8 +14,8 @@ function setup() {
 	add_action(
 		'init',
 		function () {
-			add_action( 'dt_process_distributor_attributes',    __NAMESPACE__ . '\save_post_specific_content', 10, 2 );
-			add_action( 'dt_process_subscription_attributes',   __NAMESPACE__ . '\save_post_specific_content', 10, 2 );
+			add_action( 'dt_process_distributor_attributes', __NAMESPACE__ . '\save_post_specific_content', 10, 2 );
+			add_action( 'dt_process_subscription_attributes', __NAMESPACE__ . '\save_post_specific_content', 10, 2 );
 		}
 	);
 }
@@ -29,7 +29,7 @@ function setup() {
 function save_post_specific_content( $post, $request ) {
 
 	$new_post = get_post( $post->ID );
-	$content = $new_post->post_content;
+	$content  = $new_post->post_content;
 
 	// Replace with the website local images
 	$content = preg_replace_callback(
@@ -41,7 +41,7 @@ function save_post_specific_content( $post, $request ) {
 	wp_update_post(
 		array(
 			'ID'           => $post->ID,
-			'post_content' => $content
+			'post_content' => $content,
 		)
 	);
 }
@@ -53,12 +53,12 @@ function save_post_specific_content( $post, $request ) {
  *
  * @return string
  */
-function localize_images ( $matches ) {
+function localize_images( $matches ) {
 	// Convert <img ... /> string to array consisting attributes' names as keys and their values as array of values
 	$exploded = preg_split( '/(?<=<img)\s+|(?<=")\s+/', $matches[0] );
 
 	$attrs = array();
-	foreach( $exploded as $value ) {
+	foreach ( $exploded as $value ) {
 		$t              = explode( '=', $value );
 		$attrs[ $t[0] ] = isset( $t[1] ) ? explode( ' ', $t[1] ) : array();
 	}
@@ -72,46 +72,46 @@ function localize_images ( $matches ) {
 
 	// Get attachment size and original id
 	$orig_img_id = 0;
-	$img_size = '';
-	if( ! empty( $attrs['class'] ) ) {
+	$img_size    = '';
+	if ( ! empty( $attrs['class'] ) ) {
 		foreach ( $attrs['class'] as $c ) {
 			preg_match( '/wp-image-(\d+)/', $c, $m );
-			if( ! empty( $m[1] ) ) {
+			if ( ! empty( $m[1] ) ) {
 				$orig_img_id = $m[1];
 			}
 
 			preg_match( '/size-(\w+)/', $c, $m2 );
-			if( ! empty( $m2 ) ) {
+			if ( ! empty( $m2 ) ) {
 				$img_size = $m2[1];
 			}
 		}
 	}
 
-	if( 0 === $orig_img_id ) {
+	if ( 0 === $orig_img_id ) {
 		return $matches[0];
 	}
 
 	// Get the mapped image id
 	$mapped_img_id = get_distributed_image_id( $orig_img_id );
 
-	if( $mapped_img_id === null ) {
+	if ( null === $mapped_img_id ) {
 		return $matches[0];
 	}
 
-	if( '' === $img_size ) {
+	if ( '' === $img_size ) {
 		$img_src = wp_get_attachment_url( $mapped_img_id );
 	} else {
 		$img_src = wp_get_attachment_image_url( $mapped_img_id, $img_size );
 	}
 
 	// Replace attachment url
-	if( ! empty( $attrs['src'] ) ) {
+	if ( ! empty( $attrs['src'] ) ) {
 		$attrs['src'][0] = $img_src;
 	}
 
 	// Replace image id with the appropriate one in the class
 	foreach ( $attrs['class'] as &$v ) {
-		if( strpos( $v, 'wp-image-' ) !== false ) {
+		if ( strpos( $v, 'wp-image-' ) !== false ) {
 			$v = 'wp-image-' . $mapped_img_id;
 		}
 	}
@@ -128,12 +128,12 @@ function localize_images ( $matches ) {
 	$img_tag = '';
 	foreach ( $attrs as $attr_name => $attr_values ) {
 		$img_tag .= $attr_name;
-		if( ! empty( $attr_values ) ) {
+		if ( ! empty( $attr_values ) ) {
 			$img_tag .= '="';
-			foreach( $attr_values as $attr_value ) {
+			foreach ( $attr_values as $attr_value ) {
 				$img_tag .= trim( $attr_value, '"' ) . ' ';
 			}
-			$img_tag = rtrim( $img_tag );
+			$img_tag  = rtrim( $img_tag );
 			$img_tag .= '"';
 		}
 
@@ -154,20 +154,25 @@ function get_distributed_image_id( $id ) {
 	global $wpdb;
 	$result = $wpdb->get_col( $wpdb->prepare( "SELECT post_id from $wpdb->postmeta WHERE meta_key = 'dt_original_media_id' AND meta_value = %d", $id ) );
 
-	if( 1 === count( $result ) ) {
+	if ( 1 === count( $result ) ) {
 		return $result[0];
 	}
 
-	if( count( $result ) > 1 ) {
+	if ( count( $result ) > 1 ) {
 		// We have a trashed post, dodge it
-		return $wpdb->get_var( $wpdb->prepare( " SELECT post_id 
+		return $wpdb->get_var(
+			$wpdb->prepare(
+				" SELECT post_id 
 				                                        FROM $wpdb->postmeta 
 				                                          INNER JOIN `$wpdb->posts` 
 				                                            ON `$wpdb->posts`.`ID` = `$wpdb->postmeta`.`post_id` 
 				                                            AND `$wpdb->posts`.`post_status` != 'trash' 
 				                                        WHERE meta_key LIKE 'dt_original_media_id' 
 				                                          AND meta_value = %d 
-				                                        LIMIT 1", $id ) );
+				                                        LIMIT 1",
+				$id
+			)
+		);
 	}
 
 	return null;
