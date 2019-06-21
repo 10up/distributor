bed14b613fd18c04bebcd905b3564d2cb926ba74
avag <avag.novembit@gmail.com>
2019-06-21 19:09:07 +0400

    do not apply 'send notifiaction allow in background' filter when 'doing cron'

--- includes/subscriptions.php
+++ includes/subscriptions.php
@@ -238,16 +238,18 @@ function send_notifications( $post_id ) {
 		return;
 	}
 
-	/**
-	 * Add possibility to send notification in background
-	 *
-	 * @param bool true     Whether send notification in background or not
-	 * @param int  $post_id The post id
-	 */
-	$send_notification_in_background = apply_filters( 'dt_send_notification_allow_in_background', false, $post_id );
+	if( ! wp_doing_cron() ) {
+		/**
+		 * Add possibility to send notification in background
+		 *
+		 * @param bool true     Whether send notification in background or not
+		 * @param int $post_id The post id
+		 */
+		$send_notification_in_background = apply_filters( 'dt_send_notification_allow_in_background', false, $post_id );
 
-	if ( true === $send_notification_in_background ) {
-		return;
+		if ( true === $send_notification_in_background ) {
+			return;
+		}
 	}
 
 	if( ! current_user_can( 'edit_post', $post_id ) ) {
