6b29cf7d1267628e9313276bd751fac6856ea172
Tung Du <dinhtungdu@gmail.com>
2020-06-18 18:58:28 +0700

    test: try refresh before adding content

--- tests/wpacceptance/BlocksTest.php
+++ tests/wpacceptance/BlocksTest.php
@@ -14,6 +14,8 @@ class BlocksTests extends \TestCase {
 	public function addContentToTestPost( $I ) {
 		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
 
+		$I->refresh();
+
 		try {
 			$I->getElement( '.editor-default-block-appender__content' );
 			$I->waitUntilElementVisible( '.editor-default-block-appender__content' );
@@ -24,6 +26,7 @@ class BlocksTests extends \TestCase {
 		$this->disableFullscreenEditor( $I );
 
 		$this->dismissNUXTip( $I );
+
 		usleep( 500 );
 
 		try {
@@ -32,6 +35,7 @@ class BlocksTests extends \TestCase {
 		} catch( \Exception $e ) {
 			$I->getPage()->type( '.block-editor-default-block-appender__content', 'Lorem ipsum dolor sit amet.', [ 'delay' => 10 ] );
 		}
+
 		$I->jsClick( '.editor-post-publish-button' );
 
 		try {
@@ -46,31 +50,6 @@ class BlocksTests extends \TestCase {
 	 * Test network pushing content with blocks.
 	 */
 	public function testBlocksNetworkPushedContent() {
-		$I = $this->openBrowserPage();
-
-		$I->loginAs( 'wpsnapshots' );
-		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
-
-		// Only test for the block editor.
-		if ( ! $this->editorHasBlocks( $I ) ) {
-			return;
-		}
-		$this->addContentToTestPost( $I );
-		$post_info = $this->pushPost( $I, 40, 2 );
-
-		// Now let's navigate to the new post
-		$I->moveTo( $post_info['distributed_edit_url'] );
-		$I->waitUntilElementVisible( '#wpadminbar' );
-
-		// Check that the blocks are intact by looking for the paragraph comment.
-		$source = $I->getPageSource();
-
-		$this->assertTrue(
-			(bool) preg_match( '<!-- wp:paragraph -->', stripslashes( $source ) ),
-			'Blocks were not pushed properly over an external connection'
-		);
-	}
-	public function testBlocksNetworkPushedContent2() {
 		$I = $this->openBrowserPage();
 
 		$I->loginAs( 'wpsnapshots' );
