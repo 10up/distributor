261c54f26d320aba50000697bfc13b70ec84beca
10upbot on GitHub <10upbot+github@10up.com>
2020-11-20 16:56:16 +0000

    Committing built version of 3f1ff2885ba3c7e925e6a0a5a769f4e26c316644

--- includes/external-connection-cpt.php
+++ includes/external-connection-cpt.php
@@ -281,10 +281,11 @@ function admin_enqueue_scripts( $hook ) {
 				'good_connection'           => esc_html__( 'Connection established.', 'distributor' ),
 				'limited_connection'        => esc_html__( 'Limited connection established.', 'distributor' ),
 				'no_push'                   => esc_html__( 'Push distribution unavailable.', 'distributor' ),
+				'no_permissions'            => esc_html__( 'Authentication succeeded but your account does not have permissions to create posts on the external site.', 'distributor' ),
 				'pull_limited'              => esc_html__( 'Pull distribution limited to basic content, i.e. title and content body.', 'distributor' ),
 				'endpoint_suggestion'       => esc_html__( 'Did you mean: ', 'distributor' ),
 				'endpoint_checking_message' => esc_html__( 'Checking endpoint...', 'distributor' ),
-				'bad_auth'                  => esc_html__( 'Authentication failed.', 'distributor' ),
+				'bad_auth'                  => esc_html__( 'Authentication failed due to invalid credentials.', 'distributor' ),
 				'change'                    => esc_html__( 'Change', 'distributor' ),
 				'cancel'                    => esc_html__( 'Cancel', 'distributor' ),
 				'invalid_url'               => esc_html__( 'Please enter a valid URL, including the HTTP(S).', 'distributor' ),
@@ -444,6 +445,15 @@ function meta_box_external_connection_details( $post ) {
 		$external_connection_url = '';
 	}
 
+	$external_connection_status = get_post_meta( $post->ID, 'dt_external_connections', true );
+
+	$post_types = get_post_types(
+		array(
+			'show_in_rest' => true,
+		),
+		'objects'
+	);
+
 	$registered_external_connection_types = \Distributor\Connections::factory()->get_registered();
 
 	foreach ( $registered_external_connection_types as $slug => $class ) {
@@ -469,7 +479,7 @@ function meta_box_external_connection_details( $post ) {
 		?>
 		<div class="updated is-dismissible error">
 			<p>
-				<?php esc_html_e( 'Authorization rejected, please try again.', 'distributor' ); ?>
+		<?php esc_html_e( 'Authorization rejected, please try again.', 'distributor' ); ?>
 			</p>
 		</div>
 		<?php
@@ -516,6 +526,34 @@ function meta_box_external_connection_details( $post ) {
 		<ul class="endpoint-errors"></ul>
 	</div>
 
+	<?php if ( ! empty( $external_connection_status ) ) : ?>
+		<div class="post-types-permissions hide-until-authed">
+			<h4><?php esc_html_e( 'Post types permissions', 'distributor' ); ?></h4>
+
+			<table class="wp-list-table widefat">
+				<thead>
+					<th><?php esc_html_e( 'Post types', 'distributor' ); ?></th>
+					<th><?php esc_html_e( 'Can pull?', 'distributor' ); ?></th>
+					<th><?php esc_html_e( 'Can push?', 'distributor' ); ?></th>
+				</thead>
+				<tbody>
+					<?php foreach ( $post_types as $post_type ) : ?>
+						<?php
+						if ( 'dt_subscription' === $post_type->name ) {
+							continue;
+						}
+						?>
+						<tr>
+							<td><?php echo esc_html( $post_type->label ); ?></td>
+							<td><?php echo in_array( $post_type->name, $external_connection_status['can_get'] ) ? esc_html__( 'Yes', 'distributor' ) : esc_html__( 'No', 'distributor' ); ?></td>
+							<td><?php echo in_array( $post_type->name, $external_connection_status['can_post'] ) ? esc_html__( 'Yes', 'distributor' ) : esc_html__( 'No', 'distributor' ); ?></td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	<?php endif; ?>
+
 	<fieldset class="dt-roles-allowed hide-until-authed">
 		<legend><?php esc_html_e( 'Roles Allowed to Push', 'distributor' ); ?></legend>
 
@@ -557,7 +595,7 @@ function meta_box_external_connection_details( $post ) {
 function dashboard() {
 	global $connection_list_table;
 
-	$_GET['post_type'] = 'dt_ext_connection';
+	$_GET['post_type']     = 'dt_ext_connection';
 	$_REQUEST['all_posts'] = true; // Default to replacite "All" tab
 
 	$connection_list_table->prepare_items();
@@ -736,14 +774,14 @@ function filter_post_updated_messages( $messages ) {
  *
  * @param string $site_url Remote site URL.
  *
- * @return false|string
+ * @return false|string|WP_Error
  */
 function get_rest_url( $site_url ) {
 
 	$source = wp_remote_get( $site_url );
 
 	if ( is_wp_error( $source ) ) {
-		return false;
+		return $source;
 	}
 
 	$dom = new \DOMDocument();
@@ -757,7 +795,7 @@ function get_rest_url( $site_url ) {
 		}
 	}
 
-	return false;
+	return new \WP_Error( 'rest_api_uri_not_found', __( 'The external site is private or not a WordPress site.', 'distributor' ) );
 }
 
 /**
@@ -776,8 +814,8 @@ function get_remote_distributor_info() {
 
 	$rest_url = get_rest_url( $_POST['url'] );
 
-	if ( ! $rest_url ) {
-		wp_send_json_error();
+	if ( is_wp_error( $rest_url ) ) {
+		wp_send_json_error( $rest_url );
 		exit;
 	}
 
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Distributor ===
+Contributors: 10up
+Tags: content, distribution, syndication, management,
+Requires at least: 4.7
+Tested up to: 5.4.2
+Requires PHP: 5.6
+Stable tag: 1.6.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Distributor makes it easy to distribute and reuse content across your websites — whether in a single multisite or across the web.
+
+== Description ==
+
+**Features**
+
+* Distributor supports safe, SEO-friendly content reuse and sharing via "pushing" and "pulling".
+* While logged in and editing or viewing any single post (or custom post type) that can be distributed, a `Distributor` admin bar item will appear, that will facilitate sharing ("pushing") that content to any `connection`.
+* In the admin dashboard, a top level Distributor menu item links to the "pull" screen. Here, editors can share ("pull") content from any `connection` into the current site.
+* Content this is distributed (via Push or Pull) is connected to the original. Reposted content receives updates from the original, canonical source automatically.
+
+**Connections**
+
+There are two connection types: `internal` and `external`.
+
+* Internal connections are other sites inside of the same multisite network. Any user logged into the network can distribute any content in the network to any other sites in the network where that user has permission to publish posts (assuming the site supports the same post type).
+* External connections are external websites, connected by the JSON REST API. External connections can be added in the WordPress admin dashboard under `Distributor` > `External Connections`. Administrators can decide which user roles are allowed to distribute content to and from that connection (Editors and Administrators by default). All users with those roles will inherit the permissions of the user account used to establish the remote connection.
+* External connections require HTTP Basic Authentication or [WordPress.com OAuth2](https://developer.wordpress.com/docs/oauth2/) (must be on VIP) be set up on the remote website. For Basic Auth, we recommend the [Application Passwords](https://wordpress.org/plugins/application-passwords/) plugin.
+* For external connections, Distributor needs to be installed on BOTH sides of the connection.
