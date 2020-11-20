6582cd0955619d13689a8fad5ff3225de93f0e1f
Helen Hou-Sandi <helen.y.hou@gmail.com>
2020-11-19 21:42:53 -0600

    Keep -dev in version string

--- distributor.php
+++ distributor.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'DT_VERSION', '1.6.1' );
+define( 'DT_VERSION', '1.6.1-dev' );
 define( 'DT_PLUGIN_FILE', preg_replace( '#^.*plugins/(.*)$#i', '$1', __FILE__ ) );
 
 // Define a constant if we're network activated to allow plugin to respond accordingly.
