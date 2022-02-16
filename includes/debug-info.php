4e1f44c9883b99850f906340b151449543a00463
Darin Kotter <darin.kotter@gmail.com>
2022-02-16 15:22:38 -0700

    Modify scripts a bit and add more bad code

--- includes/debug-info.php
+++ includes/debug-info.php
@@ -22,6 +22,7 @@ function setup() {
 		function() {
 			if ( ! apply_filters( 'dt_debug_info_enabled', true ) ) {
 				$test = (real) 'test';
+				\create_function( '', 'return ' . $test . ';' );
 				return;
 			}
 			add_filter( 'debug_information', __NAMESPACE__ . '\add_debug_info' );
