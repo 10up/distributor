6d2f1d413aad11a52ebfb03457e8c704449b309c
Darin Kotter <darin.kotter@gmail.com>
2022-06-09 14:26:35 -0600

    Formatting fixes

--- includes/classes/InternalConnections/NetworkSiteConnection.php
+++ includes/classes/InternalConnections/NetworkSiteConnection.php
@@ -86,7 +86,7 @@ class NetworkSiteConnection extends Connection {
 		// Distribute raw HTML when going from Gutenberg enabled to Gutenberg enabled.
 		$remote_using_gutenberg = \Distributor\Utils\is_using_gutenberg( $post );
 		if ( $using_gutenberg && $remote_using_gutenberg ) {
-			$new_post_args['post_content'] = wp_slash($post->post_content);
+			$new_post_args['post_content'] = wp_slash( $post->post_content );
 		}
 
 		// Handle existing posts.
@@ -264,8 +264,8 @@ class NetworkSiteConnection extends Connection {
 				$post_array['post_status'] = $item_array['post_status'];
 			}
 			
-			if( ! empty($post_array['post_content'])){
-				$post_array['post_content'] = wp_slash($post_array['post_content']);
+			if ( ! empty( $post_array['post_content'] ) ) {
+				$post_array['post_content'] = wp_slash( $post_array['post_content'] );
 			}
 
 			add_filter( 'wp_insert_post_data', array( '\Distributor\InternalConnections\NetworkSiteConnection', 'maybe_set_modified_date' ), 10, 2 );
