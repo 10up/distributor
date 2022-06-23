1906b06f067d7e1ecf303e38a791d4119a48a5e8
faisal-alvi <faisalalvi33@gmail.com>
2022-06-23 14:57:05 +0530

    using `activatePlugin` and `deactivatePlugin`

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,9 +1,10 @@
 describe( 'Admin can login and make sure plugin is activated', () => {
-	it( 'Can activate plugin if it is deactivated', () => {
+	before( () => {
 		cy.login();
-		cy.visit( '/wp-admin/plugins.php' );
-		cy.get( '#deactivate-distributor' ).click();
-		cy.get( '#activate-distributor' ).click();
-		cy.get( '#deactivate-distributor' ).should( 'be.visible' );
+	} );
+
+	it( 'Can deactivate and activate plugin ', () => {
+		cy.deactivatePlugin( 'classifai' );
+		cy.activatePlugin( 'classifai' );
 	} );
 } );
