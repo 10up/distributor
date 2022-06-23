131a3d730094461c9b750659d3a2a687b6c8967a
faisal-alvi <faisalalvi33@gmail.com>
2022-06-23 14:51:58 +0530

    adding de/activate plugin test

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,15 +1,8 @@
-describe("Admin can login and open dashboard", () => {
-  before(() => {
-    cy.login();
-  });
-
-  it("Open dashboard", () => {
-    cy.visit(`/wp-admin`);
-    cy.get("h1").should("contain", "Dashboard");
-  });
-
-  it("Activate Hello Dolly and deactivate it back", () => {
-    cy.activatePlugin("hello-dolly");
-    cy.deactivatePlugin("hello-dolly");
-  });
-});
+describe( 'Admin can login and make sure plugin is activated', () => {
+	it( 'Can activate plugin if it is deactivated', () => {
+		cy.visitAdminPage( 'plugins.php' );
+		cy.get( '#deactivate-distributor' ).click();
+		cy.get( '#activate-distributor' ).click();
+		cy.get( '#deactivate-distributor' ).should( 'be.visible' );
+	} );
+} );
