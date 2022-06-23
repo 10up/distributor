57eb2e8d1b2f5788b3179ed84fb258dfa2372cd2
faisal-alvi <faisalalvi33@gmail.com>
2022-06-23 14:54:38 +0530

    using the util function to visit the page

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,6 +1,7 @@
 describe( 'Admin can login and make sure plugin is activated', () => {
 	it( 'Can activate plugin if it is deactivated', () => {
-		cy.visitAdminPage( 'plugins.php' );
+		cy.login();
+		cy.visit( '/wp-admin/plugins.php' );
 		cy.get( '#deactivate-distributor' ).click();
 		cy.get( '#activate-distributor' ).click();
 		cy.get( '#deactivate-distributor' ).should( 'be.visible' );
