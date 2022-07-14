397228062c8c0677d4af7d45dc078ec792db7a75
faisal-alvi <faisalalvi33@gmail.com>
2022-07-14 15:25:50 +0530

    add new tests + remove custom plugin

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,10 +1,9 @@
 describe( 'Admin can login and make sure plugin is activated', () => {
 	before( () => {
 		cy.login();
-		cy.visit( '/wp-admin/network/plugins.php' );
 	} );
 
-	it( 'Can deactivate and activate plugin ', () => {
+	it( 'Can deactivate and activate plugin?', () => {
 		cy.deactivatePlugin( 'distributor' );
 		cy.activatePlugin( 'distributor' );
 	} );
