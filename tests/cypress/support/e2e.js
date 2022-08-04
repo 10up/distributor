f06d95f1aff6cff562a8da60ebc4ed6d344dbfb4
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-08-04 12:09:55 +1000

    Auto format tests for WP JS coding standards.

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -13,14 +13,14 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import "@10up/cypress-wp-utils";
+import '@10up/cypress-wp-utils';
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
+import './commands';
 
 // Preserve WP cookies.
-beforeEach(() => {
-  Cypress.Cookies.defaults({
-    preserve: /^wordpress.*?/,
-  });
-});
+beforeEach( () => {
+	Cypress.Cookies.defaults( {
+		preserve: /^wordpress.*?/,
+	} );
+} );
