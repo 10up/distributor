104ba2322578d4ddb686cefd411db667af3f3437
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-08-05 12:47:10 +1000

    Force name of admin-pull-table CSS file.

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,23 @@ const { resolve } = require( 'path' );
  * CSS only entry points are indicated with the suffix `-css` in the entry. This
  * suffix is removed from the file name to avoid the tortologist `-css.css`.
  *
+ * In some cases the CSS file name differs from the JavaScript filename. In a
+ * possibly foolhardy attemt to maintain backward compatibility, these values are
+ * hard coded here.
+ *
  * @param {Object} pathData Entry point path data.
  * @return {string} Resolved file name.
  */
 const cssFileName = ( pathData ) => {
 	let name = pathData.chunk.name;
 	name = name.replace( /\-css$/, '' );
+
+	// Backward compatibility for divergent file names.
+	switch ( name ) {
+		case 'admin-pull':
+			name = 'admin-pull-table';
+			break;
+	}
 
 	return `css/${ name }.min.css`;
 };
