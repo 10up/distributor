bd40f49c8a9cc1b4072c8972c98977b21e6b4203
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-09-19 14:20:48 +0300

    Move network and sub-site creating to the pre-script

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e
+
 npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
 npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+
+status=0
+npm run env run tests-cli "wp site list" || status=$?
+
+if [ $status -eq 0 ]
+then
+	echo "Multisite already initialized"
+else
+	echo "Converting to multisite"
+	npm run env run tests-cli "wp core multisite-convert --title='Distributor Multisite'"
+	npm run env run tests-cli "wp site create --slug=second --title='Second Site' --email='second@admin.local'"
+fi
