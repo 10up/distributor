cd1f41d49ad45839d291435e3478d939a29cbcd6
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-11-14 09:34:21 +1100

    Shell of the post abstraction.

---
+++ includes/classes/DistributorPost.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Distributor post object abstraction.
+ *
+ * @package  distributor
+ */
+
+namespace Distributor;
+
+/**
+ * This is the post abstraction for distributor posts.
+ *
+ * Although this class is within the Distributor namespace, the class itself
+ * includes the phrase Distributor to make it clear to developers `use`ing
+ * the class that they are not using the `WP_Post` object.
+ *
+ * @since x.x.x
+ */
+class DistributorPost {
+
+}
