c4b0b0b0ac184de7fdcadd34e05e6e4cfeb873d4
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-11-14 11:27:02 +1100

    Populate distribution details

--- includes/classes/DistributorPost.php
+++ includes/classes/DistributorPost.php
@@ -56,6 +56,46 @@ class DistributorPost {
 	public $media = [];
 
 	/**
+	 * Whether this is the source (true) or a distributed post (false).
+	 *
+	 * @var bool
+	 */
+	public $is_source = true;
+
+	/**
+	 * Whether this post is linked to the original version.
+	 *
+	 * For the original post this is set to true.
+	 *
+	 * @var bool
+	 */
+	public $is_linked = true;
+
+	/**
+	 * The original post ID.
+	 *
+	 * @var int
+	 */
+	public $original_post_id = 0;
+
+	/**
+	 * The type of connection this post is distributed from.
+	 *
+	 * @var string internal|external|empty (for source)
+	 */
+	public $connection_type = '';
+
+	/**
+	 * The connection ID this post is distributed from.
+	 *
+	 * For internal connections this is the site ID. For external connections
+	 * this refers to the connection ID.
+	 *
+	 * @var int
+	 */
+	public $connection_id = 0;
+
+	/**
 	 * Initialize the DistributorPost object.
 	 *
 	 * @param WP_Post|int $post WordPress post object or post ID.
@@ -73,5 +113,40 @@ class DistributorPost {
 		$this->meta  = Utils\prepare_meta( $post->ID );
 		$this->terms = Utils\prepare_taxonomy_terms( $post->ID );
 		$this->media = Utils\prepare_media( $post->ID );
+
+		/*
+		 * The original post ID is listed as excluded post meta and therefore
+		 * unavailable in the meta property. We need to get it using the post
+		 * meta API.
+		 */
+		$original_post_id = get_post_meta( $post->ID, 'dt_original_post_id', true );
+		if ( empty( $original_post_id ) ) {
+			// This is the source post.
+			$this->is_source = true;
+			$this->is_linked = true;
+			return;
+		}
+
+		// Set up information for a distributed post.
+		$this->is_source = false;
+		// Reverse value of the `dt_unlinked` meta data.
+		$this->is_linked = ! get_post_meta( $post->ID, 'dt_unlinked', true );
+
+		// Determine the connection type.
+		if ( get_post_meta( $post->ID, 'dt_original_blog_id', true ) ) {
+			// Internal connections store the original blog's ID.
+			$this->connection_type = 'internal';
+			$this->connection_id   = get_post_meta( $post->ID, 'dt_original_blog_id', true );
+		} else {
+			$this->connection_type = 'external';
+			$this->connection_id   = get_post_meta( $post->ID, 'dt_original_source_id', true );
+
+			// Pushed posts have a `dt_subscription_signature` meta key.
+			if ( ! $this->connection_id && get_post_meta( $post->ID, 'dt_subscription_signature', true ) ) {
+				// The post was pushed.
+				$this->connection_type = 'push';
+				$this->connection_id   = get_post_meta( $post->ID, 'dt_subscription_signature', true );
+			}
+		}
 	}
 }
