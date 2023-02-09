c4ea0f8a8076c3d51410c50a0ef69fd070edf87d
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-02-09 22:46:22 +0530

    add: create pot file with wp cli
    I create a bash script which uses package,json -> files to create pot file for addon.

---
+++ bin/generate-pot-with-wpcli.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script generates a POT file for a WordPress plugin or theme using WP-CLI.
+# We are using wp-scripts to generate plugin zip which bundle files defined in 'package.json' in files param.
+# https://developer.wordpress.org/block-editor/packages/packages-scripts/#files
+# This script will run with npm run makepot command.
+
+if ! command -v wp &>/dev/null; then
+	echo "Error: wp cli could not be found. Please install wp cli and try again."
+	exit
+fi
+
+# Get the list of files and directories which bundle in final zip file.
+FILES=$(node -p "require('./package.json').files.join(',')")
+
+# Run the WP-CLI command.
+# This command will generate a POT file in the root directory of the plugin and store it in land directory.
+wp i18n make-pot . --include="$FILES"
+
+echo '.pot file updated'
