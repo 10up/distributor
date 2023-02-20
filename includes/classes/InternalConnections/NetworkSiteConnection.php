84c3f03657de0e4b288c4f7e4a78be89a276117d
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-02-20 14:12:17 +1100

    Use post abstraction for pulling network posts.

--- includes/classes/InternalConnections/NetworkSiteConnection.php
+++ includes/classes/InternalConnections/NetworkSiteConnection.php
@@ -256,14 +256,24 @@ class NetworkSiteConnection extends Connection {
 		$created_posts = array();
 
 		foreach ( $items as $item_array ) {
-			$post = $this->remote_get( [ 'id' => $item_array['remote_post_id'] ] );
+			$update      = false;
+			$insert_args = array();
+			if ( ! empty( $item_array['post_status'] ) ) {
+				$insert_args['post_status'] = $item_array['post_status'];
+			}
+			if ( ! empty( $item_array['post_id'] ) ) {
+				$insert_args['remote_post_id'] = $item_array['post_id'];
+				$update                        = true;
+			}
+
+			$post = $this->remote_get( [ 'id' => $item_array['remote_post_id'] ], $insert_args );
 
 			if ( is_wp_error( $post ) ) {
 				$created_posts[] = $post;
 				continue;
 			}
 
-			$post_props      = get_object_vars( $post );
+			$post_props      = $post;
 			$post_array      = array();
 			$current_blog_id = get_current_blog_id();
 
@@ -278,41 +288,20 @@ class NetworkSiteConnection extends Connection {
 				}
 			}
 
-			foreach ( $post_props as $key => $value ) {
-				$post_array[ $key ] = $value;
-			}
-
-			if ( ! empty( $item_array['post_id'] ) ) {
-				$post_array['ID'] = $item_array['post_id'];
-			} else {
-				unset( $post_array['ID'] );
-			}
-
-			if ( isset( $post_array['post_parent'] ) ) {
-				unset( $post_array['post_parent'] );
-			}
-
-			if ( ! empty( $item_array['post_status'] ) ) {
-				$post_array['post_status'] = $item_array['post_status'];
-			}
-
 			add_filter( 'wp_insert_post_data', array( '\Distributor\InternalConnections\NetworkSiteConnection', 'maybe_set_modified_date' ), 10, 2 );
 
 			// Filter documented in includes/classes/ExternalConnections/WordPressExternalConnection.php
 			$new_post_args = Utils\post_args_allow_list( apply_filters( 'dt_pull_post_args', $post_array, $item_array['remote_post_id'], $post, $this ) );
-			$new_post_id   = wp_insert_post( wp_slash( $new_post_args ) );
-
+			if ( $update ) {
+				$new_post_id = wp_update_post( wp_slash( $new_post_args ) );
+			} else {
+				$new_post_id = wp_insert_post( wp_slash( $new_post_args ) );
+			}
 			remove_filter( 'wp_insert_post_data', array( '\Distributor\InternalConnections\NetworkSiteConnection', 'maybe_set_modified_date' ), 10, 2 );
 
 			if ( ! is_wp_error( $new_post_id ) ) {
-				update_post_meta( $new_post_id, 'dt_original_post_id', (int) $item_array['remote_post_id'] );
 				update_post_meta( $new_post_id, 'dt_original_blog_id', (int) $this->site->blog_id );
 				update_post_meta( $new_post_id, 'dt_syndicate_time', time() );
-				update_post_meta( $new_post_id, 'dt_original_post_url', esc_url_raw( $post->link ) );
-
-				if ( ! empty( $post->post_parent ) ) {
-					update_post_meta( $new_post_id, 'dt_original_post_parent', (int) $post->post_parent );
-				}
 
 				/**
 				 * Allow bypassing of all meta processing.
@@ -321,15 +310,15 @@ class NetworkSiteConnection extends Connection {
 				 *
 				 * @param {bool}                  true            If Distributor should set the post meta.
 				 * @param {int}                   $new_post_id    The newly created post ID.
-				 * @param {array}                 $post->meta     List of meta items attached to the post, formatted by {@link \Distributor\Utils\prepare_meta()}.
+				 * @param {array}                 $post_meta      List of meta items attached to the post, formatted by {@link \Distributor\Utils\prepare_meta()}.
 				 * @param {int}                   $remote_post_id The original post ID.
 				 * @param {array}                 $post_array     The arguments passed into wp_insert_post.
 				 * @param {NetworkSiteConnection} $this           The Distributor connection being pulled from.
 				 *
 				 * @return {bool} If Distributor should set the post meta.
 				 */
-				if ( apply_filters( 'dt_pull_post_meta', true, $new_post_id, $post->meta, $item_array['remote_post_id'], $post_array, $this ) ) {
-					\Distributor\Utils\set_meta( $new_post_id, $post->meta );
+				if ( apply_filters( 'dt_pull_post_meta', true, $new_post_id, $post['meta'], $item_array['remote_post_id'], $post_array, $this ) ) {
+					\Distributor\Utils\set_meta( $new_post_id, $post['meta'] );
 				}
 
 				/**
@@ -339,15 +328,15 @@ class NetworkSiteConnection extends Connection {
 				 *
 				 * @param {bool}                  true            If Distributor should set the post terms.
 				 * @param {int}                   $new_post_id    The newly created post ID.
-				 * @param {array}                 $post->terms    List of terms items attached to the post, formatted by {@link \Distributor\Utils\prepare_taxonomy_terms()}.
+				 * @param {array}                 $post_terms     List of terms items attached to the post, formatted by {@link \Distributor\Utils\prepare_taxonomy_terms()}.
 				 * @param {int}                   $remote_post_id The original post ID.
 				 * @param {array}                 $post_array     The arguments passed into wp_insert_post.
 				 * @param {NetworkSiteConnection} $this           The Distributor connection being pulled from.
 				 *
 				 * @return {bool} If Distributor should set the post terms.
 				 */
-				if ( apply_filters( 'dt_pull_post_terms', true, $new_post_id, $post->terms, $item_array['remote_post_id'], $post_array, $this ) ) {
-					\Distributor\Utils\set_taxonomy_terms( $new_post_id, $post->terms );
+				if ( apply_filters( 'dt_pull_post_terms', true, $new_post_id, $post['terms'], $item_array['remote_post_id'], $post_array, $this ) ) {
+					\Distributor\Utils\set_taxonomy_terms( $new_post_id, $post['terms'] );
 				}
 
 				/**
@@ -357,15 +346,15 @@ class NetworkSiteConnection extends Connection {
 				 *
 				 * @param {bool}                  true            If Distributor should set the post media.
 				 * @param {int}                   $new_post_id    The newly created post ID.
-				 * @param {array}                 $post->media    List of media items attached to the post, formatted by {@link \Distributor\Utils\prepare_media()}.
+				 * @param {array}                 $post_media     List of media items attached to the post, formatted by {@link \Distributor\Utils\prepare_media()}.
 				 * @param {int}                   $remote_post_id The original post ID.
 				 * @param {array}                 $post_array     The arguments passed into wp_insert_post.
 				 * @param {NetworkSiteConnection} $this           The Distributor connection being pulled from.
 				 *
 				 * @return {bool} If Distributor should set the post media.
 				 */
-				if ( apply_filters( 'dt_pull_post_media', true, $new_post_id, $post->media, $item_array['remote_post_id'], $post_array, $this ) ) {
-					\Distributor\Utils\set_media( $new_post_id, $post->media, [ 'use_filesystem' => true ] );
+				if ( apply_filters( 'dt_pull_post_media', true, $new_post_id, $post['media'], $item_array['remote_post_id'], $post_array, $this ) ) {
+					\Distributor\Utils\set_media( $new_post_id, $post['media'], [ 'use_filesystem' => true ] );
 				};
 			}
 
@@ -504,11 +493,18 @@ class NetworkSiteConnection extends Connection {
 	/**
 	 * Remotely get posts so we can list them for pulling
 	 *
-	 * @param  array $args Array of args for getting.
 	 * @since  0.8
+	 * @since 2.0.0 Added $new_post_args parameter.
+	 *
+	 * @param  array $args Array of args for getting.
+	 * @param  array $new_post_args {
+	 *    Array of args for creating new post.
+	 *
+	 *    @type string $post_status Post status for new post.
+	 * }
 	 * @return array|WP_Post|bool
 	 */
-	public function remote_get( $args = array() ) {
+	public function remote_get( $args = array(), $new_post_args = array() ) {
 
 		$id = ( empty( $args['id'] ) ) ? false : $args['id'];
 
@@ -593,7 +589,8 @@ class NetworkSiteConnection extends Connection {
 			if ( empty( $post ) ) {
 				$formatted_post = false;
 			} else {
-				$formatted_post = Utils\prepare_post( $post );
+				$dt_post        = new DistributorPost( $post );
+				$formatted_post = $dt_post->to_insert( $new_post_args );
 			}
 
 			restore_current_blog();
