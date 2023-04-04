a6db663a42bd951056182c3e02a522e1c793aad2
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-04-04 15:02:09 +1000

    Always use POST requests on pull screen.

--- includes/classes/ExternalConnections/WordPressExternalConnection.php
+++ includes/classes/ExternalConnections/WordPressExternalConnection.php
@@ -167,8 +167,8 @@ class WordPressExternalConnection extends ExternalConnection {
 			}
 		}
 
-		// When running a query for the Pull screen with excluded items, make a POST request instead
-		if ( empty( $id ) && isset( $args['post__not_in'] ) && isset( $args['dt_pull_list'] ) ) {
+		// When running a query for the Pull screen, make a POST request instead
+		if ( empty( $id ) && isset( $args['dt_pull_list'] ) ) {
 			$query_args['post_type']      = isset( $post_type ) ? $post_type : 'post';
 			$query_args['posts_per_page'] = isset( $posts_per_page ) ? $posts_per_page : 20;
 
