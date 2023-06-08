5102ac9ad22d573c7f511e796ae77209dad011c8
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-06-08 11:15:06 +1000

    Ensure unlinked posts show expected notice and are interactive.

--- tests/cypress/e2e/link-unlink.test.js
+++ tests/cypress/e2e/link-unlink.test.js
@@ -45,4 +45,45 @@ describe( 'Linking and unlinking tests', () => {
 			} );
 		} );
 	} );
+
+	it( 'Should show the linking option for unlinked posts', () => {
+		const postTitle = 'Post to distribute ' + randomName();
+
+		cy.createPost( { title: postTitle } ).then( ( sourcePost ) => {
+			// Push the post to the second network site.
+			cy.distributorPushPost(
+				sourcePost.id,
+				'second',
+				'',
+				'publish'
+			).then( ( distributedPost ) => {
+				// Open the distributed post in the second network site.
+				cy.visit( distributedPost.distributedEditUrl );
+				cy.closeWelcomeGuide();
+
+				// Unlink the post.
+				cy.get( '.components-notice__actions a:nth-child(1)' )
+					.should( 'contain.text', 'Unlink from the origin post' )
+					.click();
+
+				// Ensure the post shows link to original post.
+				cy.get( '.components-notice__content' )
+					.should(
+						'contain.text',
+						'This post has been unlinked from the origin post'
+					)
+					.should(
+						'contain.text',
+						'Edits to the origin post will not update this remote version'
+					);
+
+				// Ensure the post is editable
+				cy.get( '.edit-post-visual-editor' ).should(
+					'not.have.css',
+					'pointer-events',
+					'none'
+				);
+			} );
+		} );
+	} );
 } );
