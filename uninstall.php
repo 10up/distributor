0f8eaec1c4a3b77d466ca22e5fbe2d871a29e4a0
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-08-04 17:00:47 +0530

    refactor: remove unnecessary code

--- uninstall.php
+++ uninstall.php
@@ -10,14 +10,6 @@ if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
-if ( is_multisite() && wp_is_large_network() ) {
-	delete_site_option( 'dt_settings' );
-	delete_site_option( 'dt_sync_log' );
-} else {
-	delete_option( 'dt_settings' );
-	delete_option( 'dt_sync_log' );
-}
-
 // Delete options.
 $wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'dt\_%';" );
 
