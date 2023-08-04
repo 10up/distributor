620f8efe5697d47ad54147ba997e710d43f69f37
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-08-04 17:00:00 +0530

    fix: add prefix to transient name

--- uninstall.php
+++ uninstall.php
@@ -17,3 +17,13 @@ if ( is_multisite() && wp_is_large_network() ) {
 	delete_option( 'dt_settings' );
 	delete_option( 'dt_sync_log' );
 }
+
+// Delete options.
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'dt\_%';" );
+
+// Remove transients.
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_transients\_dt\_%';" );
+
+// Delete our data from the post and post meta tables.
+$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'dt_subscription', 'dt_ext_connection' );" );
+$wpdb->query( "DELETE meta FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE posts.ID IS NULL;" );
