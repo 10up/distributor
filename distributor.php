11cd51b4ca72b3639154b0fa354ffe799c7b342f
Darin Kotter <darin.kotter@gmail.com>
2023-11-17 14:10:50 -0700

    Version bump to 2.0.2

--- distributor.php
+++ distributor.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://github.com/10up/distributor
  * Update URI:        https://distributorplugin.com
  * Description:       Makes it easy to distribute and reuse content across your websites, whether inside of a multisite or across the web.
- * Version:           2.0.1
+ * Version:           2.0.2
  * Requires at least: 5.7
  * Requires PHP:      7.4
  * Author:            10up Inc.
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'DT_VERSION', '2.0.1' );
+define( 'DT_VERSION', '2.0.2' );
 define( 'DT_PLUGIN_FILE', preg_replace( '#^.*plugins/(.*)$#i', '$1', __FILE__ ) );
 define( 'DT_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DT_PLUGIN_FULL_FILE', __FILE__ );
