6c36276ab0ac8c63ea89e5b41ce6c4077e42e64a
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-07-15 03:26:28 +0530

    try fixing e2e

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-set -e
+# set -e
 
-npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
-npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+# npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
+# npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
 
-status=0
-npm run env run tests-cli "wp site list" || status=$?
+# status=0
+# npm run env run tests-cli "wp site list" || status=$?
 
-if [ $status -eq 0 ]
-then
-	echo "Multisite already initialized"
-else
-	echo "Converting to multisite"
-	npm run env run tests-cli "wp core multisite-convert --title='Distributor Multisite'"
-	npm run env run tests-cli "wp user create second 'second@admin.local' --user_pass=password --role=administrator"
-	npm run env run tests-cli "wp site create --slug=second --title='Second Site' --email='second@admin.local'"
-	npm run env run tests-cli "wp theme enable twentytwentyone --activate"
-	npm run env run tests-cli "wp theme enable twentytwentyone --url=localhost/second --activate"
-	npm run env run tests-cli "cp wp-content/plugins/distributor/tests/cypress/.htaccess .htaccess"
-fi
+# if [ $status -eq 0 ]
+# then
+# 	echo "Multisite already initialized"
+# else
+# 	echo "Converting to multisite"
+# 	npm run env run tests-cli "wp core multisite-convert --title='Distributor Multisite'"
+# 	npm run env run tests-cli "wp user create second 'second@admin.local' --user_pass=password --role=administrator"
+# 	npm run env run tests-cli "wp site create --slug=second --title='Second Site' --email='second@admin.local'"
+# 	npm run env run tests-cli "wp theme enable twentytwentyone --activate"
+# 	npm run env run tests-cli "wp theme enable twentytwentyone --url=localhost/second --activate"
+# 	npm run env run tests-cli "cp wp-content/plugins/distributor/tests/cypress/.htaccess .htaccess"
+# fi
+
+wp-env run cli wp core multisite-convert
+HTACCESS_PATH="$(wp-env install-path)/WordPress/.htaccess"
+cp ./tests/bin/.htaccess $HTACCESS_PATH;
+wp-env run cli wp user create second 'second@admin.local' --user_pass=password --role=administrator
+wp-env run cli wp site create --slug=second --title='Second Site' --email='second@admin.local'
