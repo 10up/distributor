9efce6995427e57ffdf6bd53100ff84f9168d240
Peter Wilson <git-commit-by@peterwilson.cc>
2025-08-19 09:34:33 +1000

    Use image that exists.

--- tests/cypress/e2e/internal-push.test.js
+++ tests/cypress/e2e/internal-push.test.js
@@ -59,7 +59,7 @@ describe( 'Internal Push', () => {
 			);
 
 			// Set Featured Image
-			cy.uploadMedia( 'assets/img/banner-772x250.png' ).then(
+			cy.uploadMedia( 'assets/img/browser-frame.jpg' ).then(
 				( media ) => {
 					if ( media && media.mediaId ) {
 						cy.wpCli(
