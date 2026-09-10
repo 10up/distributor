0a7481029fa015dd6f66de2779b212786d4d3864
Peter Wilson <git-commit-by@peterwilson.cc>
2026-09-10 11:38:14 +1000

    Add messages to existing tests.

--- tests/php-wp-integration/tests/test_debuginfo.php
+++ tests/php-wp-integration/tests/test_debuginfo.php
@@ -16,10 +16,11 @@ use WP_UnitTestCase;
 class Test_DebugInfo extends WP_UnitTestCase {
 	public function test_add_debug_info() {
 		$info = DebugInfo\add_debug_info( [] );
-		$this->assertArrayHasKey( 'distributor', $info );
-		$this->assertArrayHasKey( 'label', $info['distributor'] );
-		$this->assertArrayHasKey( 'fields', $info['distributor'] );
-		$this->assertEquals( 'Distributor', $info['distributor']['label'] );
-		$this->assertEquals( 6, count( $info['distributor']['fields'] ) );
+
+		$this->assertArrayHasKey( 'distributor', $info, 'Debug info should contain distributor.' );
+		$this->assertArrayHasKey( 'label', $info['distributor'], 'Distributor info should include label.' );
+		$this->assertArrayHasKey( 'fields', $info['distributor'], 'Distributor info should include fields' );
+		$this->assertEquals( 'Distributor', $info['distributor']['label'], 'Distributor label should be Distributor.' );
+		$this->assertEquals( 6, count( $info['distributor']['fields'] ), 'Debug info should include six fields.' );
 	}
 }
